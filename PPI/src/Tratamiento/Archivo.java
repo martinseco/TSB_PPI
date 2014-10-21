e4dcff44a36dbf37914aba67fcb74e990bbe1381
martinseco <mseco.93@gmail.com>
2014-10-21 18:17:57 -0300

    Inicial 2

--- PPI/src/Tratamiento/Archivo.java
+++ PPI/src/Tratamiento/Archivo.java
@@ -45,8 +45,8 @@ public class Archivo {
         
         Scanner sc = new Scanner(f);
 //        sc.useDelimiter(" ").useDelimiter(".").useDelimiter(",");
-        Pattern patron = Pattern.compile("[0-9][ \\t\\n\\x0B\\f\\r]$");
-        sc.useDelimiter(patron);
+        
+        sc.useDelimiter("[^a-zA-ZñÑá-úÁ-Ú]");
         
         while(sc.hasNext()){
             String palabra = sc.next();
@@ -59,89 +59,7 @@ public class Archivo {
                 contadorPalabras.put(palabra, i+1);
         }
         return contadorPalabras;
-    }
-    
-    
-    
-    /*Obtengo un HashMap a partir de una ResultSet que obtengo de una consulta.
-      Tendria que obtener una de estas por cada tabla que deseo consultar, para verificar si 
-      es INSERT o si debo realizar un UPDATE.
-    
-    HASHMAP DE LO QUE YA EXISTE EN LA PERSISTENCIA.
-    */
-    /*Situo el cursor en determinada fila y de alli obtengo los 2 objetos y los inserto en el HashMap 
-    en la misma posicion en que la recupero de la ResultSet.
-    */
-    private HashMap mapaBD(ResultSet rs) throws SQLException{
-    
-        HashMap resultado = new HashMap();
-        
-        while(rs.next()){//Movimiento entre las filas de la tabla.
-        
-            resultado.put(rs.getObject(1),rs.getObject(2));//Obtengo objeto en las columnas 1 y 2 de la fila en la 
-                                                            // que estoy parado.
-        }
-        
-        return resultado;
-    }
-    
-    
-    
-    
-    /*Obtengo el ResultSet de la consulta. Tendré que hacer tres consultas, una por cada tabla;es decir, 
-    tres invocaciones a este metodo.
-    */
-    private ResultSet tablaConsulta(String tabla) throws SQLException{
-    
-        Conexion con = new Conexion();
-        ResultSet resultado = con.tabla(tabla);
-        con.cerrarConexiones();
-        
-        return resultado;
-    }
-    
-    
-    
-    
-    /*Comparar los 2 HashMaps  y determinar si la clave ya existe, de no existir, esto determinaría un INSERT,
-    de existir sería un UPDATE.
-    Voy a crear 2 HashMaps, uno determina el HashMap que respondera a la consulta INSERT y el otro
-    el que respondera a la consulta UPDATE.
-    */
-    public HashMap[] compararMapas() throws SQLException, FileNotFoundException{
-    //[0] = UPDATE; [1] = INSERT;
-        HashMap[] resultado = new HashMap[2];
-        HashMap bd;
-        HashMap archivo;
-        
-        bd = this.mapaBD(this.tablaConsulta("Palabra"));
-        archivo = this.mapaPalabras();
-        
-        Set claves = archivo.keySet();
-        Iterator it = claves.iterator();
-        Object key;
-        Object value;
-        int insercion;
-        
-        while(it.hasNext()){
-            key = it.next();//Obtengo las claves de todo el mapa.
-            
-            if (bd.containsKey(key)) {//Ya esta en la BD
-                insercion = 0;
-                }
-            else
-                    insercion = 1; //No esta en la BD
-            
-        value = bd.get(key);//Obtengo el valor para la clave asociada.
-        resultado[insercion].put(key, value); //Inserto en el HashMap correspondiente.       
-            
-        }
-        
-        return resultado;
-    }
-    
-    
-    
+    }   
     
 }
 
