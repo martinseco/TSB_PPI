9ed84bd0e0f366655084cb12fcf79796230667cc
LuchoCruz <luchocruz23@gmail.com>
2014-10-23 13:28:14 -0300

    BdFInal Inserciones DOC y PAL

--- PPI/src/BD/Insercion.java
+++ PPI/src/BD/Insercion.java
@@ -8,9 +8,13 @@ package BD;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,7 +25,7 @@ public class Insercion {
     private String nombreDoc;
     private Statement stmt;
     private Connection c;
-    
+    private HashMap <String, Integer> palabrasArchivo;
     
     public Insercion(String nombre){
     
@@ -30,39 +34,152 @@ public class Insercion {
     
     public void conectar() throws SQLException{
         
-        c = DriverManager.getConnection("jdbc:sqlite:BD.s3db");//Conexion con BD abierta.
+        c = DriverManager.getConnection("jdbc:sqlite:DBFinal.db");//Conexion con BD abierta.
+        c.setAutoCommit(false);
         stmt = c.createStatement();
     }
     
     public void procesar(HashMap contadorPalabras) throws SQLException{
         
         this.conectar();
-        HashMap <String, Integer> palabrasArchivo  = contadorPalabras;
+        palabrasArchivo  = contadorPalabras;//Ver si va en el constructor o no.
         String sql = "";
+        int idDoc;
+        int idPal;
         
         try
         {
-            c.setAutoCommit(false);
+            idDoc = this.getId(nombreDoc, "Documento");
+            if(idDoc == -1)
+            {   
+                sql = this.insertDocumento();
+                stmt.executeUpdate(sql);
+            }            
+            
             for (String key : palabrasArchivo.keySet()) 
-            {            
-                sql = "INSERT OR REPLACE INTO Palabra (palabra, cantidad) "; 
-                sql += "VALUES ('";
-                sql += key + "',(COALESCE((SELECT cantidad FROM palabra WHERE palabra LIKE '" + key + "') + '" ;
-                sql += palabrasArchivo.get(key) + "', '" + palabrasArchivo.get(key) +"')))";
-
-                stmt.executeUpdate(sql);  
+            {  
+                key = key.toLowerCase();
+                idPal = this.getId(key, "Palabra");
+                
+                if(idPal == -1)
+                {
+                    sql = this.insertarPalabra(key);
+                    stmt.executeUpdate(sql); 
+                    
+                }
+                
             }
-            c.commit();
-            c.setAutoCommit(true);
+           
+            
+            
+            
+          c.commit();
+          c.setAutoCommit(true);
         }catch(SQLException e) 
         {
+            System.out.println("Sali po el SqlExc"+e);
             c.rollback();
         }
         
         stmt.close();
-        c.close();
-        
+        c.close();        
     }
+    
+    private int getId(String key, String tabla)
+    {
+        String sql = "";
+        int id = -1;
+        
+        if (key != null && !" ".equals(key) && tabla.equals("Palabra")) 
+        {
+            
+           ResultSet rs;
+           key = key.toLowerCase();
+            try {
+                System.out.println("Entro con: " + key);
+                rs = stmt.executeQuery("SELECT id FROM Palabra WHERE palabra = '" + key +"' LIMIT 1");
+                id = rs.getInt(1);
+                System.out.println(id);
+                }
+           catch (SQLException ex) {
+               System.out.println("Salio con: " + key);
+                id = -1;
+           }
+           //UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='Palabra'
+            
+        }
+        else
+        {
+            ResultSet rs;
+            key = key.toLowerCase();
+            try {
+                rs = stmt.executeQuery("SELECT id FROM Documento WHERE nombre = '" + key +"' LIMIT 1");
+                id = rs.getInt(1);
+            }
+           catch (SQLException ex) {
+                id = -1;
+           }            
+        }
+       return id;
+    }
+    
+//    if (id == -1) {
+//                sql = "INSERT INTO Palabra (id,palabra) "; 
+//                sql += "VALUES ($next_id, '"+key+"')";
+//            }
+    private int getLastIdPal()
+    {
+        int id;
+        ResultSet rs;
+        String sql = "SELECT MAX(id) FROM Palabra";
+        
+        try{
+            
+            rs = stmt.executeQuery(sql);
+            id = rs.getInt(1);
+        }catch(SQLException e)
+        { 
+            id = -1;
+        }
+        
+        return id;
+    }
+    
+    
+    private String insertDocumento()
+    {
+        String sql = "";
+        ResultSet rs;
+        int id;
+        
+//        try {
+//            rs = stmt.executeQuery("SELECT id FROM Documento WHERE nombre = '" + nombreDoc +"' LIMIT 1");
+//            id = rs.getInt(1); 
+//         } catch (SQLException ex) {
+//            id = -1;
+ //        }
+ //       if (id == -1) {
+            sql = "INSERT INTO Documento (id,nombre) ";
+            sql += "VALUES ($next_id, '";
+            sql += nombreDoc + "')";
+//        }
+        
+        return sql;
+    }
+    
+    private String insertarPalabra(String key)
+    {
+        String sql ="";
+        if (key != null && !" ".equals(key)) {
+             sql = "INSERT INTO Palabra (id,palabra) "; 
+             sql += "VALUES ($next_id, '"+key+"')";
+        }
+        
+       
+        return sql;
+    }
+   
+    
     
     
             
